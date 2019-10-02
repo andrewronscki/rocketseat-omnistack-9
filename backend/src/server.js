583f2086af27ed57583d0fc64d194411de300dd4
André <andrewronscki@gmail.com>
2019-10-01 21:24:14 -0300

    Backend

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/omnistack9', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);

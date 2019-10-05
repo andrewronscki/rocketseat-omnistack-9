eb49c59346846291b0efce3831132bb2110766c9
André <andrewronscki@gmail.com>
2019-10-05 13:56:55 -0300

    Implementação de real-time com socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,34 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb://localhost:27017/omnistack9', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+// utilizar o redis
+const connectedUsers  = {};
+
+io.on('connection', socket => {
+  const {  user_id } =  socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
 });
 
 app.use(cors());
@@ -17,4 +38,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
